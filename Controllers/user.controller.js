8213c77d18e443cf8e038e73ca0e9348a705c961
duuree17 <duuren0315@gmail.com>
2023-07-25 00:00:52 +0800

    V..

---
+++ Controllers/user.controller.js
@@ -0,0 +1,76 @@
+const { userModel } = require("../Models");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const Login = async (req, res) => {
+  const user = req.body;
+  const result = await userModel.findOne({ name: user.name });
+  try {
+    if (result) {
+      const isEqual = await bcrypt.compare(user.password, result.password);
+      if (isEqual) {
+        const token = jwt.sign({ id: result._id }, process.env.ACCESS_TOKEN, {
+          expiresIn: "10h",
+        });
+        res.send(token);
+      } else {
+        res.send("Wrong pass mate");
+      }
+    } else {
+      res.send("NO ACCOUNT FOUND!!!!!!!!!!");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const addUserUrl = async (req, res) => {
+  const { userId, urlId } = req.body;
+  try {
+    const user = await userModel.findById(userId).populate("history");
+    user.history.push(urlId);
+    await user.save();
+    res.send(user);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getUser = (req, res) => {
+  const authHeader = req.headers;
+  const token = authHeader["authorization"];
+  if (token == null) return res.send("No token found");
+  jwt.verify(token, process.env.ACCESS_TOKEN, async (err, user) => {
+    if (err) return res.send("token expired");
+    const account = await userModel
+      .findOne({ _id: user.id }, { password: 0 })
+      .populate("history");
+    res.send(account);
+  });
+};
+
+const getUsers = async (req, res) => {
+  const users = await userModel.find({}, { password: 0 });
+  res.send(users);
+};
+
+const SignUp = async (req, res) => {
+  try {
+    const { name, password } = req.body;
+    const hash = await bcrypt.hash(password, 10);
+    const user = new userModel({ name, password: hash });
+    const exists = await userModel.findOne({ name });
+    if (!exists) {
+      const result = await user.save();
+      const token = jwt.sign({ id: result._id }, process.env.ACCESS_TOKEN, {
+        expiresIn: "10h",
+      });
+      res.send(token);
+    } else {
+      res.send("User already exists");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = { addUserUrl, Login, SignUp, getUser, getUsers };
